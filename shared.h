5ab5c3f2329caeb6c40dba91d05df4b04c665eeb
Amit Ziv-Kenet <azk@ubuntu.(none)>
2012-12-29 19:23:04 +0200

    Shared header file for flex and bison

---
+++ shared.h
@@ -0,0 +1,29 @@
+#ifndef _SHARED_H_
+#define _SHARED_H_
+
+typedef enum
+{
+    LP,
+    RP,
+    LC,
+    RC,
+    SC,
+    ASSIGN,
+    PLUS,
+    MINUS,
+    MULT,
+    DIV,
+    REL_OP,
+    AND,
+    OR,
+    INT,
+    NIS,
+    AGORA,
+    PRINT,
+    INPUT,
+    ID,
+    STRING,
+} Token; 
+
+
+#endif
